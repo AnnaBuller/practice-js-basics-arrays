5de6b5faf642d84ea8304024a50c41b0b14cd976
AnnaBuller <aniabela@wp.pl>
2020-09-16 12:21:50 +0200

    fix reduce accum

--- 05/app.js
+++ 05/app.js
@@ -2,7 +2,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7];
 const evenTotal = sumEven(numbers);
 
 function sumEven(arr) {
-  return arr.filter(isEven).reduce(function(a, b) {return a + b})
+  return arr.filter(isEven).reduce(function (a, b) { return a + b }, 0);
 }
 
 function isEven(num) {
@@ -11,10 +11,10 @@ function isEven(num) {
 
 console.log(evenTotal)
 
-// Teoretycznie można powyższe skrócić jeszcze bardziej, tylko tracimy możliwość wykorzystania funkcji isEven gdzieś indziej (zmniejsza się też czytelność :P):
+// Teoretycznie można powyższe skrócić jeszcze bardziej:
 
 function sumEven(arr) {
-  return arr.filter(function(num) {return num % 2 === 0}).reduce(function(a, b) {return a + b})
+  return arr.filter(function (num) { return num % 2 === 0 }).reduce(function (a, b) { return a + b }, 0)
 }
 
 console.log(evenTotal)
